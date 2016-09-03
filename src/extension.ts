ae37d8f4c71c8cc856b80873cd584a2c4029eda8
zjhmale <zjhmale@gmail.com>
2016-09-03 10:01:08 +0800

    add sha1 encoding

---
+++ src/extension.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+import * as vscode from 'vscode';
+import * as encode from './encode'
+
+export function activate(context: vscode.ExtensionContext) {
+}
+
+export function deactivate() {
+}
